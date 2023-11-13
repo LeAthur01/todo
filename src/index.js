ae18a64ca068e4ede246961129d95b63812c74bb
LeAthur01 <147360626+LeAthur01@users.noreply.github.com>
2023-11-13 18:13:19 +0700

    Add a detail description when users click on the detail button

---
+++ src/index.js
@@ -0,0 +1,30 @@
+function popUp() {
+    const detailBtns = document.querySelectorAll('#todos .button');
+    const popUpBackground = document.querySelector('#pop-up');
+    const popUp = document.querySelector('#pop-up div');
+
+
+    detailBtns.forEach(btn => {
+        btn.addEventListener('click', e => {
+            popUpBackground.classList.toggle('hidden');
+        })
+    });
+
+    popUpBackground.addEventListener('click', e => {
+        popUpBackground.classList.toggle('hidden');
+    });
+
+    popUp.addEventListener('click', e => {
+        e.stopPropagation();
+    })
+}
+
+popUp();
+
+function toggleHidden(e) {
+
+}
+
+
+
+
